a218de8bf4929d0f2c16821ca602b20c98bed052
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2020-05-10 17:39:34 +0200

    Update build.sh

--- srv6_controller/protos/build.sh
+++ srv6_controller/protos/build.sh
@@ -4,7 +4,7 @@ SERVICE=.
 
 DESTDIR='gen-py'
 mkdir -p $DESTDIR
-python -m grpc_tools.protoc \
+python3 -m grpc_tools.protoc \
     --proto_path=$SERVICE/ \
     --python_out=$DESTDIR \
     --grpc_python_out=$DESTDIR \
