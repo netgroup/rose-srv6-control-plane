4565cad52d9bd1b1ce457d4d2845593456c1890e
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2020-06-28 11:44:50 +0200

    Clean conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,9 +15,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../db_update'))
 
 
 # -- Project information -----------------------------------------------------
