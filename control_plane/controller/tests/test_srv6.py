466abdac38988f10979e4bef5a111654d96612e2
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2020-07-20 13:48:06 +0200

    Fix pytests

--- control_plane/controller/tests/test_srv6.py
+++ control_plane/controller/tests/test_srv6.py
@@ -29,8 +29,10 @@ def test_nodes_to_addrs():
     addrs_list = ['fcbb:bb00:0001::',
                   'fcbb:bb00:0002::',
                   'fcbb:bb00:0003::']
-    assert normalize_ip_addrs(srv6_utils.nodes_to_addrs(
-        nodes, NODES_YAML)[0]) == normalize_ip_addrs(addrs_list), 'Test failed'
+    nodes_info = srv6_utils.read_nodes(NODES_YAML)[0]
+    assert normalize_ip_addrs([node['uN'] for node in nodes_info.values()
+                               if node['name'] in nodes]) \
+        == normalize_ip_addrs(addrs_list), 'Test failed'
 
 
 def test_segments_to_micro_segment_1():
@@ -118,7 +120,7 @@ def test_sidlist_to_usidlist_2():
         'FCBB:BB00:0005::',
         'FCBB:BB00:0006::',
     ]
-    usid_list = ['fcbb:bb00:0001:0002:0003:0004:0005:0006']
+    usid_list = ['fcbb:bb00:0001:0002:0003:0004:0005::', 'fcbb:bb00:0006::']
     assert normalize_ip_addrs(srv6_utils.sidlist_to_usidlist(sid_list)) == \
         normalize_ip_addrs(usid_list)
 
@@ -136,8 +138,8 @@ def test_sidlist_to_usidlist_3():
         'FCBB:BB00:0008::',
     ]
     usid_list = [
-        'fcbb:bb00:0001:0002:0003:0004:0005:0006',
-        'fcbb:bb00:0007:0008::',
+        'fcbb:bb00:0001:0002:0003:0004:0005::',
+        'fcbb:bb00:0006:0007:0008::',
     ]
     assert normalize_ip_addrs(srv6_utils.sidlist_to_usidlist(sid_list)) == \
         normalize_ip_addrs(usid_list)
@@ -146,8 +148,8 @@ def test_sidlist_to_usidlist_3():
 def test_nodes_to_micro_segments():
     nodes = ['R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R8']
     usid_list = [
-        'fcbb:bb00:0001:0002:0003:0004:0005:0006',
-        'fcbb:bb00:0008::',
+        'fcbb:bb00:0001:0002:0003:0004:0005::',
+        'fcbb:bb00:0006:0008::',
     ]
     assert normalize_ip_addrs(srv6_utils.nodes_to_micro_segments(
         nodes, NODES_YAML)) == normalize_ip_addrs(usid_list)
