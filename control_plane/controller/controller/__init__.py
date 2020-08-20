48e44a2ceae1882e443f622319bfac9802dfad34
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2020-08-20 12:02:03 +0200

    Minor improvements

--- control_plane/controller/controller/__init__.py
+++ control_plane/controller/controller/__init__.py
@@ -18,7 +18,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Controller
+# Implementation of a SDN Controller
 #
 # @author Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
 #
